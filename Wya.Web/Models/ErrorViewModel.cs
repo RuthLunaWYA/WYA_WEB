71a596c8810c4b8dea81a9178daa4f9a0c71aebf
KleymerWYA <grupowyadesarrollo02@hotmail.com>
2025-09-19 16:24:47 -0500

    Agregar archivos de proyecto.

---
+++ Wya.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wya.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
