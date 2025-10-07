0c97e2108e06cb3be9d73866b810b0e008dddd4c
Ronald-Guti <113565299+Ronald-Gutierrez@users.noreply.github.com>
2025-10-07 17:28:00 -0500

    feat(Se han creado los controladores para poder manejar las rutas de Servicios Dedicados. Se ha creado el controlador para Servicios Integrales tambien, Se han creado los archivos para las rutas de los servicios dedicados, Se agrego los estilos para la vista de Cliente, para poder reutilizarlo)

---
+++ Wya.Web/Controllers/ServiciosDedicadosController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wya.Web.Controllers
+{
+    [Route("Servicios/Dedicados")]
+    public class ServiciosDedicadosController : Controller
+    {
+        // Ruta: /Servicios/Dedicados
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            // Si tienes una vista general de "Dedicados"
+            return View("Dedicados");
+        }
+
+        // Ruta: /Servicios/Dedicados/FibraOptica
+        [HttpGet("FibraOptica")]
+        public IActionResult FibraOptica()
+        {
+            return View("~/Views/ServiciosDedicados/FibraOptica.cshtml");
+        }
+
+        // Ruta: /Servicios/Dedicados/TarifasPlanes
+        [HttpGet("TarifasPlanes")]
+        public IActionResult TarifasPlanes()
+        {
+            return View("~/Views/ServiciosDedicados/TarifasPlanes.cshtml");
+        }
+
+        // Ruta: /Servicios/Dedicados/TelefoniaVoip
+        [HttpGet("TelefoniaVoip")]
+        public IActionResult TelefoniaVoip()
+        {
+            return View("~/Views/ServiciosDedicados/TelefoniaVoip.cshtml");
+        }
+
+        // Ruta: /Servicios/Dedicados/CorreoZimbra
+        [HttpGet("CorreoZimbra")]
+        public IActionResult CorreoZimbra()
+        {
+            return View("~/Views/ServiciosDedicados/CorreoZimbra.cshtml");
+        }
+    }
+}
---
+++ Wya.Web/Controllers/ServiciosIntegralesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wya.Web.Controllers
+{
+    [Route("Servicios")]
+    public class ServiciosIntegralesController : Controller
+    {
+        [Route("Integrales")]
+        public IActionResult Integrales()
+        {
+            // Carga la vista: Views/Servicios/Integrales.cshtml
+            return View("Integrales");
+        }
+    }
+}
